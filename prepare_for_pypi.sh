d77dc08d051179486492d64a53a74f096ba3dec4
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2026-09-22 21:01:28 +0200

    Remove debug echo statements and upgrade local libraries command from prepare_for_pypi.sh

--- prepare_for_pypi.sh
+++ prepare_for_pypi.sh
@@ -17,15 +17,9 @@ ptig manage_schdevtools makeallmigrations
 ptig manage_schdevtools migrate
 ptig manage_schdevtools createautouser
 ptig manage_schdevtools import_projects
-echo "X1"
-ptig manage_schpytigondemo upgradelocallibs
-echo "X2"
 ptig manage_schpytigondemo makeallmigrations
-echo "X3"
 ptig manage_schpytigondemo migrate
-echo "X4"
 ptig manage_schpytigondemo createautouser
-echo "X5"
 ptig manage_schmanage makeallmigrations
 ptig manage_schmanage migrate
 ptig manage_schmanage createautouser
